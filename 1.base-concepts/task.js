0131af053f85d16f569ad2557edc0beee9c1c523
Mariia <nissin2008@yandex.ru>
2021-07-19 18:34:07 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,14 @@
+'use strict';
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
+  let D = b ** 2 - 4 * a * c;
+  let arr = [];
+
+  if (D > 0) {
+    arr[0] = -b + Math.sqrt(D) / (2 * a);
+    arr[1] = -b - Math.sqrt(D) / (2 * a);
+  } else if (D === 0) {
+    arr[0] = -b + Math.sqrt(D) / (2 * a);
+  } 
   return arr; // array
-}
-
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
-
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
 }
