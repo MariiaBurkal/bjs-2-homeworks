d72211b2479bb6b4706b723b597c328b6d57c303
Mariia <nissin2008@yandex.ru>
2021-07-30 13:30:15 +0300

    Update task.js

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,11 @@
 function compareArrays(arr1, arr2) {
   let result;
 
-  // Ваш код
+   if (arr1.length === arr2.length && arr1.every((n, i) => n === arr2[i])) {
+    result = true;
+  } else {
+    result = false;
+  }
 
   return result; // boolean
 }
@@ -9,7 +13,10 @@ function compareArrays(arr1, arr2) {
 function advancedFilter(arr) {
   let resultArr;
 
-  // Ваш код
+  resultArr = arr
+    .filter((item) => item > 0)
+    .filter((item) => item % 3 === 0)
+    .map((item) => item * 10);
 
   return resultArr; // array
 }
